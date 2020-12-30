6dae563e419d3421e586317485e8ebf3b833df60
Sriharsha Mucheli <harshasrisri@gmail.com>
2020-12-29 23:58:43 -0800

    Step 6 - Disable raw mode at exit

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,89 @@
 use std::io::{self, Read, Result, Error, ErrorKind};
-use libc::{termios as Termios, c_int};
+use libc::{termios as Termios, c_int, tcflag_t};
 use std::mem;
 
-fn disable_echo() -> Result<()> {
-    extern "C" {
-        pub fn tcgetattr(fd: c_int, termios: *mut Termios) -> c_int; 
-        pub fn tcsetattr(fd: c_int, optional_actions: c_int, termios: *const Termios) -> c_int;
+trait TermiosAttrExt {
+    fn get_attr(&mut self) -> Result<()>;
+    fn set_attr(&self) -> Result<()>;
+}
+
+impl TermiosAttrExt for Termios {
+    fn get_attr(&mut self) -> Result<()> {
+        Ok( unsafe {
+            if tcgetattr(libc::STDIN_FILENO, self) != 0 {
+                return Err(Error::new(ErrorKind::Other, "Can't get term attributes"));
+            }
+        })
     }
-    unsafe {
-        let mut termio: Termios = mem::zeroed();
-        if tcgetattr(libc::STDIN_FILENO, &mut termio) != 0 {
-            return Err(Error::new(ErrorKind::Other, "Can't get term attributes"));
-        }
-        termio.c_lflag &= !(libc::ECHO);
-        if tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &termio) != 0 {
-            return Err(Error::new(ErrorKind::Other, "Can't set term attributes"));
-        }
+
+    fn set_attr(&self) -> Result<()> {
+        Ok( unsafe {
+            if tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, self) != 0 {
+                return Err(Error::new(ErrorKind::Other, "Can't get term attributes"));
+            }
+        })
     }
-    Ok(())
+}
+
+enum TermioFlagFields {
+    InputFlags,
+    OutputFlags,
+    ControlFlags,
+    LocalFlags,
+}
+
+extern "C" {
+    pub fn tcgetattr(fd: c_int, termios: *mut Termios) -> c_int; 
+    pub fn tcsetattr(fd: c_int, optional_actions: c_int, termios: *const Termios) -> c_int;
+}
+
+struct Terminal {
+    orig_flags: Termios,
+    curr_flags: Termios,
+}
+
+impl Terminal {
+    pub fn new() -> Result<Self> {
+        let (mut orig_flags, mut curr_flags) = unsafe {(mem::zeroed::<Termios>(), mem::zeroed::<Termios>())};
+        orig_flags.get_attr()?;
+        curr_flags.get_attr()?;
+        Ok(Self { orig_flags, curr_flags })
+    }
+
+    pub fn enable_flag(&mut self, field: TermioFlagFields, flag: tcflag_t) -> Result<()> {
+        let curr_field = match field {
+            TermioFlagFields::InputFlags   => &mut self.curr_flags.c_iflag,
+            TermioFlagFields::OutputFlags  => &mut self.curr_flags.c_oflag,
+            TermioFlagFields::ControlFlags => &mut self.curr_flags.c_cflag,
+            TermioFlagFields::LocalFlags   => &mut self.curr_flags.c_lflag,
+        };
+        *curr_field |= flag as u32;
+        self.curr_flags.set_attr()?;
+        Ok(())
+    }
+
+    pub fn disable_flag(&mut self, field: TermioFlagFields, flag: tcflag_t) -> Result<()> {
+        let curr_field = match field {
+            TermioFlagFields::InputFlags   => &mut self.curr_flags.c_iflag,
+            TermioFlagFields::OutputFlags  => &mut self.curr_flags.c_oflag,
+            TermioFlagFields::ControlFlags => &mut self.curr_flags.c_cflag,
+            TermioFlagFields::LocalFlags   => &mut self.curr_flags.c_lflag,
+        };
+        *curr_field &= !(flag as u32);
+        self.curr_flags.set_attr()?;
+        Ok(())
+    }
+}
+
+impl Drop for Terminal {
+    fn drop(&mut self) {
+        self.orig_flags.set_attr().expect("Failed to restore terminal state");
+    }
 }
 
 fn main() -> Result<()> {
-    disable_echo()?;
+    let mut terminal = Terminal::new()?;
+    terminal.disable_flag(TermioFlagFields::LocalFlags, libc::ECHO)?;
     let mut buf = [0; 1];
     loop {
         let n = io::stdin().read(&mut buf)?;
