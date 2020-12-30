80c69699aac6d3c62219628a923c4ddc67442280
Sriharsha Mucheli <harshasrisri@gmail.com>
2020-12-29 22:44:21 -0800

    Step 5 - Turn off echoing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
-use std::io::{self, Read};
+use std::io::{self, Read, Result, Error, ErrorKind};
+use libc::{termios as Termios, c_int};
+use std::mem;
 
-fn main() -> io::Result<()> {
-    let mut stdin = io::stdin();
+fn disable_echo() -> Result<()> {
+    extern "C" {
+        pub fn tcgetattr(fd: c_int, termios: *mut Termios) -> c_int; 
+        pub fn tcsetattr(fd: c_int, optional_actions: c_int, termios: *const Termios) -> c_int;
+    }
+    unsafe {
+        let mut termio: Termios = mem::zeroed();
+        if tcgetattr(libc::STDIN_FILENO, &mut termio) != 0 {
+            return Err(Error::new(ErrorKind::Other, "Can't get term attributes"));
+        }
+        termio.c_lflag &= !(libc::ECHO);
+        if tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &termio) != 0 {
+            return Err(Error::new(ErrorKind::Other, "Can't set term attributes"));
+        }
+    }
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    disable_echo()?;
     let mut buf = [0; 1];
     loop {
-        let n = stdin.read(&mut buf)?;
+        let n = io::stdin().read(&mut buf)?;
         if n == 0 || buf[0] == b'q' || buf[0] == b'Q' {
             return Ok(());
         }
