cdc9f0cb3b5fb043395c5c5a392852121bb79bb7
Sriharsha Mucheli <harshasrisri@gmail.com>
2021-02-17 14:05:05 -0800

    Step 130.1 - Major (fearless) refactor. Detach buffer handling to separate module

---
+++ src/buffer.rs
@@ -0,0 +1,205 @@
+use crate::line::Line;
+use crate::terminal::Motion;
+use std::cmp::min;
+use std::path::PathBuf;
+
+#[derive(Default)]
+pub struct Buffer {
+    render_col: usize,
+    cursor_col: usize,
+    cursor_row: usize,
+    lines: Vec<Line>,
+    row_offset: usize,
+    col_offset: usize,
+    filename: Option<PathBuf>,
+    dirty: bool,
+}
+
+impl Buffer {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn is_dirty(&self) -> bool {
+        self.dirty
+    }
+
+    pub fn not_dirty(&mut self) {
+        self.dirty = false;
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.lines.is_empty()
+    }
+
+    pub fn filename(&self) -> &Option<PathBuf> {
+        &self.filename
+    }
+
+    pub fn set_filename(&mut self, filename: Option<String>) {
+        self.filename = filename.map(|filename| filename.into());
+    }
+
+    pub fn line_count(&self) -> usize {
+        self.lines.len()
+    }
+
+    pub fn cursor_position(&self) -> (usize, usize) {
+        (self.cursor_row, self.cursor_col)
+    }
+
+    pub fn cursor_placement(&self) -> (usize, usize) {
+        (
+            self.cursor_row - self.row_offset + 1,
+            self.render_col - self.col_offset + 1,
+        )
+    }
+
+    pub fn move_cursor(&mut self, motion: Motion, rows: usize, cols: usize) {
+        match motion {
+            Motion::Up => self.cursor_row = self.cursor_row.saturating_sub(1),
+            Motion::Left => {
+                if self.cursor_col != 0 {
+                    self.cursor_col -= 1;
+                } else if self.cursor_row > 0 {
+                    self.cursor_row -= 1;
+                    self.cursor_col = self.lines[self.cursor_row].len();
+                }
+            }
+            Motion::Down => {
+                self.cursor_row = min(self.lines.len().saturating_sub(1), self.cursor_row + 1)
+            }
+            Motion::Right => {
+                if let Some(row) = self.lines.get(self.cursor_row) {
+                    if self.cursor_col < row.len() {
+                        self.cursor_col += 1;
+                    } else if self.cursor_row < self.lines.len() - 1 {
+                        self.cursor_row += 1;
+                        self.cursor_col = 0;
+                    }
+                }
+            }
+            Motion::PgUp => self.cursor_row = self.cursor_row.saturating_sub(rows),
+            Motion::PgDn => {
+                self.cursor_row = min(self.lines.len().saturating_sub(1), self.cursor_row + rows)
+            }
+            Motion::Home => self.cursor_col = 0,
+            Motion::End => self.cursor_col = cols - 1,
+        }
+
+        if let Some(row) = self.lines.get(self.cursor_row) {
+            self.cursor_col = min(row.len(), self.cursor_col);
+        }
+    }
+
+    pub fn scroll(&mut self, rows: usize, cols: usize) {
+        self.render_col = self
+            .lines
+            .get(self.cursor_row)
+            .map(|line| line.render_position(self.cursor_col))
+            .unwrap_or_default();
+
+        if self.cursor_row < self.row_offset {
+            self.row_offset = self.cursor_row;
+        } else if self.cursor_row >= self.row_offset + rows {
+            self.row_offset = 1 + self.cursor_row - rows;
+        }
+
+        if self.render_col < self.col_offset {
+            self.col_offset = self.render_col;
+        } else if self.render_col >= self.col_offset + cols {
+            self.col_offset = 1 + self.render_col - cols;
+        }
+    }
+
+    pub fn frame_content(&self, rows: usize, cols: usize) -> String {
+        self.lines
+            .iter()
+            .skip(self.row_offset)
+            .map(|line| line.rendered())
+            .chain(
+                std::iter::repeat("~")
+                    .take(rows.saturating_sub(self.lines.len().saturating_sub(self.row_offset))),
+            )
+            .map(|line| {
+                line.chars()
+                    .skip(self.col_offset)
+                    .take(cols)
+                    .chain("\x1b[K\r\n".chars())
+            })
+            .take(rows)
+            .flatten()
+            .collect::<String>()
+    }
+
+    pub fn rows_to_string(&self) -> String {
+        let mut content = self
+            .lines
+            .iter()
+            .map(|line| line.content().to_string())
+            .collect::<Vec<String>>()
+            .join("\n");
+        content.push('\n');
+        content
+    }
+
+    fn insert_row(&mut self, index: usize, line: String) {
+        if index > self.lines.len() {
+            return;
+        }
+        self.lines.insert(index, Line::new(line));
+        self.dirty = true;
+    }
+
+    pub fn append_row(&mut self, line: String) {
+        self.insert_row(self.lines.len(), line);
+    }
+
+    pub fn insert_new_line(&mut self) {
+        if self.cursor_col == 0 {
+            self.insert_row(self.cursor_row, String::new());
+        } else {
+            let tail = self.lines[self.cursor_row].split_off(self.cursor_col);
+            self.insert_row(self.cursor_row + 1, tail);
+        }
+        self.cursor_row += 1;
+        self.cursor_col = 0;
+    }
+
+    pub fn insert_char(&mut self, ch: char) {
+        if self.cursor_row == self.lines.len() {
+            self.insert_row(self.cursor_row, String::new());
+        }
+        if let Some(line) = self.lines.get_mut(self.cursor_row) {
+            line.insert(self.cursor_col, ch);
+            self.cursor_col += 1;
+            self.dirty = true;
+        }
+    }
+
+    fn delete_row(&mut self) {
+        if self.cursor_row < self.lines.len() {
+            self.lines.remove(self.cursor_row);
+            self.dirty = true;
+        }
+    }
+
+    pub fn delete_char(&mut self) {
+        if (self.cursor_row, self.cursor_col) == (0, 0) {
+            return;
+        }
+        if let Some(line) = self.lines.get_mut(self.cursor_row) {
+            if self.cursor_col > 0 {
+                line.remove(self.cursor_col - 1);
+                self.cursor_col -= 1;
+                self.dirty = true;
+            } else {
+                self.cursor_col = self.lines[self.cursor_row - 1].len();
+                let tail = self.lines[self.cursor_row].content().to_string();
+                self.lines[self.cursor_row - 1].push_str(&tail);
+                self.delete_row();
+                self.cursor_row -= 1;
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 use core::format_args;
-use std::cmp::min;
 use std::fmt::Write;
 use std::fs::File;
 use std::io::Result;
 use std::io::{BufRead, BufReader};
-use std::path::PathBuf;
 use std::time::Instant;
-use txtdt::line::Line;
+use txtdt::buffer::Buffer;
 use txtdt::terminal::{Key, Motion, Terminal};
 
 const STATUS_HEIGHT: usize = 2; // 1 for Status bar. 1 for Status Message
@@ -23,35 +21,19 @@ macro_rules! editor_set_status_message {
     }}
 }
 
-struct EditorState {
+struct Editor {
     terminal: Terminal,
-    render_col: usize,
-    cursor_col: usize,
-    cursor_row: usize,
-    keep_alive: bool,
-    lines: Vec<Line>,
-    row_offset: usize,
-    col_offset: usize,
-    filename: Option<PathBuf>,
-    dirty: bool,
+    buffer: Buffer,
     status_msg: String,
     status_msg_ts: Instant,
     quit_count: usize,
 }
 
-impl EditorState {
+impl Editor {
     pub fn new() -> Result<Self> {
         Ok(Self {
             terminal: Terminal::new_raw()?,
-            render_col: 0,
-            cursor_col: 0,
-            cursor_row: 0,
-            keep_alive: true,
-            lines: Vec::new(),
-            row_offset: 0,
-            col_offset: 0,
-            dirty: false,
-            filename: None,
+            buffer: Buffer::new(),
             status_msg: String::new(),
             status_msg_ts: Instant::now(),
             quit_count: TOTAL_QUIT_COUNT,
@@ -65,70 +47,37 @@ impl EditorState {
     pub fn cols(&self) -> usize {
         self.terminal.cols()
     }
-}
 
-fn editor_move_cursor(e: &mut EditorState, motion: Motion) {
-    match motion {
-        Motion::Up => e.cursor_row = e.cursor_row.saturating_sub(1),
-        Motion::Left => {
-            if e.cursor_col != 0 {
-                e.cursor_col -= 1;
-            } else if e.cursor_row > 0 {
-                e.cursor_row -= 1;
-                e.cursor_col = e.lines[e.cursor_row].len();
-            }
-        }
-        Motion::Down => e.cursor_row = min(e.lines.len().saturating_sub(1), e.cursor_row + 1),
-        Motion::Right => {
-            if let Some(row) = e.lines.get(e.cursor_row) {
-                if e.cursor_col < row.len() {
-                    e.cursor_col += 1;
-                } else if e.cursor_row < e.lines.len() - 1 {
-                    e.cursor_row += 1;
-                    e.cursor_col = 0;
-                }
-            }
-        }
-        Motion::PgUp => e.cursor_row = e.cursor_row.saturating_sub(e.rows()),
-        Motion::PgDn => {
-            e.cursor_row = min(e.lines.len().saturating_sub(1), e.cursor_row + e.rows())
-        }
-        Motion::Home => e.cursor_col = 0,
-        Motion::End => e.cursor_col = e.cols() - 1,
-    }
-
-    if let Some(row) = e.lines.get(e.cursor_row) {
-        e.cursor_col = min(row.len(), e.cursor_col);
+    pub fn keep_alive(&self) -> bool {
+        self.quit_count > 0
     }
 }
 
-fn editor_process_keypress(e: &mut EditorState) -> Result<()> {
+fn editor_process_keypress(e: &mut Editor) -> Result<()> {
     let key = e.terminal.read_key()?;
 
     match key {
         Key::Control('Q') => {
-            if e.dirty && e.quit_count > 0 {
+            if e.buffer.is_dirty() && e.quit_count > 0 {
                 editor_set_status_message!(
                     e,
                     "WARNING!!! Press Ctrl-Q {} more times to quit. File has unsaved changes.",
                     e.quit_count
                 );
                 e.quit_count -= 1;
-            } else {
-                e.keep_alive = false;
             }
             return Ok(()); // To prevent resetting QUIT_COUNT
         }
         Key::Control('S') => editor_save(e)?,
-        Key::Move(motion) => editor_move_cursor(e, motion),
-        Key::Printable(ch) => editor_insert_char(e, ch),
-        Key::Tab => editor_insert_char(e, '\t'),
-        Key::Newline => editor_insert_new_line(e),
+        Key::Move(motion) => e.buffer.move_cursor(motion, e.rows(), e.cols()),
+        Key::Printable(ch) => e.buffer.insert_char(ch),
+        Key::Tab => e.buffer.insert_char('\t'),
+        Key::Newline => e.buffer.insert_new_line(),
         Key::Escape | Key::Control('L') => {}
-        Key::Backspace | Key::Control('H') => editor_delete_char(e),
+        Key::Backspace | Key::Control('H') => e.buffer.delete_char(),
         Key::Delete => {
-            editor_move_cursor(e, Motion::Right);
-            editor_delete_char(e);
+            e.buffer.move_cursor(Motion::Right, e.rows(), e.cols());
+            e.buffer.delete_char();
         }
         _key => {}
     };
@@ -136,7 +85,7 @@ fn editor_process_keypress(e: &mut EditorState) -> Result<()> {
     Ok(())
 }
 
-fn editor_draw_home_screen(e: &mut EditorState) {
+fn editor_home_screen(e: &Editor) -> String {
     let mut banner = format!(
         "{} -- version {}",
         env!("CARGO_PKG_NAME"),
@@ -153,57 +102,30 @@ fn editor_draw_home_screen(e: &mut EditorState) {
         centered
     };
 
-    e.terminal.append(
-        std::iter::repeat("~")
-            .take(e.rows())
-            .enumerate()
-            .map(|(n, buf)| {
-                if n == e.rows() / 3 {
-                    banner.chars()
-                } else {
-                    buf.chars()
-                }
-            })
-            .flat_map(|buf| buf.chain("\x1b[K\r\n".chars()))
-            .collect::<String>()
-            .as_str(),
-    );
+    std::iter::repeat("~")
+        .take(e.rows())
+        .enumerate()
+        .map(|(n, buf)| {
+            if n == e.rows() / 3 {
+                banner.chars()
+            } else {
+                buf.chars()
+            }
+        })
+        .flat_map(|buf| buf.chain("\x1b[K\r\n".chars()))
+        .collect()
 }
 
-fn editor_draw_content(e: &mut EditorState) {
-    e.terminal.append(
-        e.lines
-            .iter()
-            .skip(e.row_offset)
-            .map(|line| line.rendered())
-            .chain(
-                std::iter::repeat("~").take(
-                    e.rows()
-                        .saturating_sub(e.lines.len().saturating_sub(e.row_offset)),
-                ),
-            )
-            .map(|line| {
-                line.chars()
-                    .skip(e.col_offset)
-                    .take(e.cols())
-                    .chain("\x1b[K\r\n".chars())
-            })
-            .take(e.rows())
-            .flatten()
-            .collect::<String>()
-            .as_str(),
-    );
-}
-
-fn editor_draw_rows(e: &mut EditorState) {
-    if e.lines.is_empty() {
-        editor_draw_home_screen(e)
+fn editor_draw_rows(e: &mut Editor) {
+    let content = if e.buffer.is_empty() {
+        editor_home_screen(e)
     } else {
-        editor_draw_content(e)
-    }
+        e.buffer.frame_content(e.rows(), e.cols())
+    };
+    e.terminal.append(content.as_str());
 }
 
-fn editor_prompt(e: &mut EditorState, prompt: &str) -> Result<Option<String>> {
+fn editor_prompt(e: &mut Editor, prompt: &str) -> Result<Option<String>> {
     let mut reply = String::new();
     loop {
         editor_set_status_message!(e, "{}{}", prompt, reply);
@@ -228,10 +150,10 @@ fn editor_prompt(e: &mut EditorState, prompt: &str) -> Result<Option<String>> {
     }
 }
 
-fn editor_draw_status_bar(e: &mut EditorState) {
-    e.terminal.append("\x1b[7m");
+fn editor_draw_status_bar(e: &mut Editor) {
     let filename = e
-        .filename
+        .buffer
+        .filename()
         .as_ref()
         .map(|file| file.to_str().unwrap_or("<file-name-not-utf8>"))
         .unwrap_or("[No Name]");
@@ -239,10 +161,15 @@ fn editor_draw_status_bar(e: &mut EditorState) {
         "{name:<.*} - {lc} lines {dirty}",
         FILE_NAME_WIDTH,
         name = filename,
-        lc = e.lines.len(),
-        dirty = if e.dirty { "(modified)" } else { "" },
+        lc = e.buffer.line_count(),
+        dirty = if e.buffer.is_dirty() {
+            "(modified)"
+        } else {
+            ""
+        },
     );
-    let status_right = format!("{}/{}", e.cursor_row + 1, e.lines.len());
+    let (c_row, _) = e.buffer.cursor_position();
+    let status_right = format!("{}/{}", c_row + 1, e.buffer.line_count());
     let num_spaces = e
         .cols()
         .saturating_sub(status_left.len())
@@ -255,32 +182,14 @@ fn editor_draw_status_bar(e: &mut EditorState) {
         right = status_right
     );
     status.truncate(e.cols());
+
+    e.terminal.append("\x1b[7m");
     e.terminal.append(status.as_str());
     e.terminal.append("\x1b[m");
     e.terminal.append("\r\n");
 }
 
-fn editor_scroll(e: &mut EditorState) {
-    e.render_col = e
-        .lines
-        .get(e.cursor_row)
-        .map(|line| line.render_position(e.cursor_col))
-        .unwrap_or_default();
-
-    if e.cursor_row < e.row_offset {
-        e.row_offset = e.cursor_row;
-    } else if e.cursor_row >= e.row_offset + e.rows() {
-        e.row_offset = 1 + e.cursor_row - e.rows();
-    }
-
-    if e.render_col < e.col_offset {
-        e.col_offset = e.render_col;
-    } else if e.render_col >= e.col_offset + e.cols() {
-        e.col_offset = 1 + e.render_col - e.cols();
-    }
-}
-
-fn editor_draw_message_bar(e: &mut EditorState) {
+fn editor_draw_message_bar(e: &mut Editor) {
     e.terminal.append("\x1b[K");
     if e.status_msg_ts.elapsed().as_secs() < 5 {
         e.status_msg.truncate(e.cols());
@@ -289,8 +198,8 @@ fn editor_draw_message_bar(e: &mut EditorState) {
     }
 }
 
-fn editor_refresh_screen(e: &mut EditorState) {
-    editor_scroll(e);
+fn editor_refresh_screen(e: &mut Editor) {
+    e.buffer.scroll(e.rows(), e.cols());
 
     e.terminal.append("\x1b[?25l");
     e.terminal.append("\x1b[H");
@@ -299,124 +208,52 @@ fn editor_refresh_screen(e: &mut EditorState) {
     editor_draw_status_bar(e);
     editor_draw_message_bar(e);
 
-    e.terminal.append(
-        format!(
-            "\x1b[{};{}H",
-            e.cursor_row - e.row_offset + 1,
-            e.render_col - e.col_offset + 1
-        )
-        .as_str(),
-    );
+    let (c_row, c_col) = e.buffer.cursor_placement();
+    e.terminal
+        .append(format!("\x1b[{};{}H", c_row, c_col).as_str());
     e.terminal.append("\x1b[?25h");
     e.terminal.flush();
 }
 
-fn editor_insert_new_line(e: &mut EditorState) {
-    if e.cursor_col == 0 {
-        editor_insert_row(e, e.cursor_row, String::new());
-    } else {
-        let tail = e.lines[e.cursor_row].split_off(e.cursor_col);
-        editor_insert_row(e, e.cursor_row + 1, tail);
-    }
-    e.cursor_row += 1;
-    e.cursor_col = 0;
-}
-
-fn editor_insert_row(e: &mut EditorState, index: usize, line: String) {
-    if index > e.lines.len() {
-        return;
-    }
-    e.lines.insert(index, Line::new(line));
-    e.dirty = true;
-}
-
-fn editor_insert_char(e: &mut EditorState, ch: char) {
-    if e.cursor_row == e.lines.len() {
-        editor_insert_row(e, e.cursor_row, String::new());
-    }
-    if let Some(line) = e.lines.get_mut(e.cursor_row) {
-        line.insert(e.cursor_col, ch);
-        e.cursor_col += 1;
-        e.dirty = true;
-    }
-}
-
-fn editor_delete_char(e: &mut EditorState) {
-    if (e.cursor_row, e.cursor_col) == (0, 0) {
-        return;
-    }
-    if let Some(line) = e.lines.get_mut(e.cursor_row) {
-        if e.cursor_col > 0 {
-            line.remove(e.cursor_col - 1);
-            e.cursor_col -= 1;
-            e.dirty = true;
-        } else {
-            e.cursor_col = e.lines[e.cursor_row - 1].len();
-            let tail = e.lines[e.cursor_row].content().to_string();
-            e.lines[e.cursor_row - 1].push_str(&tail);
-            editor_delete_row(e);
-            e.cursor_row -= 1;
-        }
-    }
-}
-
-fn editor_delete_row(e: &mut EditorState) {
-    if e.cursor_row < e.lines.len() {
-        e.lines.remove(e.cursor_row);
-        e.dirty = true;
-    }
-}
-
-fn editor_rows_to_string(e: &EditorState) -> String {
-    let mut content = e
-        .lines
-        .iter()
-        .map(|line| line.content().to_string())
-        .collect::<Vec<String>>()
-        .join("\n");
-    content.push('\n');
-    content
-}
-
-fn editor_save(e: &mut EditorState) -> Result<()> {
-    if e.filename.is_none() {
+fn editor_save(e: &mut Editor) -> Result<()> {
+    if e.buffer.filename().is_none() {
         if let Ok(some_name) = editor_prompt(e, "Save as (ESC to cancel): ") {
-            e.filename = some_name.map(|filename| filename.into());
+            e.buffer.set_filename(some_name);
         }
     }
-    if let Some(filename) = &e.filename {
-        let content = editor_rows_to_string(e);
+    if let Some(filename) = &e.buffer.filename() {
+        let content = e.buffer.rows_to_string();
         if let Err(err) = std::fs::write(filename, content.as_bytes()) {
             editor_set_status_message!(e, "Can't save! I/O error: {}", err);
             return Err(err);
         }
         editor_set_status_message!(e, "{} bytes written to disk", content.len());
-        e.dirty = false;
+        e.buffer.not_dirty();
     } else {
         editor_set_status_message!(e, "Filename not set!!!");
     }
     Ok(())
 }
 
-fn editor_open(e: &mut EditorState, file_arg: Option<String>) -> Result<()> {
+fn editor_open(e: &mut Editor, file_arg: Option<String>) -> Result<()> {
     if let Some(file) = file_arg {
-        e.filename = Some(file.clone().into());
+        e.buffer.set_filename(Some(file.clone()));
         let line_iter = BufReader::new(File::open(file)?).lines();
         for line in line_iter {
-            editor_insert_row(e, e.lines.len(), line?);
+            e.buffer.append_row(line?);
         }
     }
-    e.dirty = false;
+    e.buffer.not_dirty();
     Ok(())
 }
 
 fn main() -> Result<()> {
-    let mut editor = EditorState::new()?;
+    let mut editor = Editor::new()?;
 
     editor_open(&mut editor, std::env::args().nth(1))?;
     editor_set_status_message!(&mut editor, "HELP: Ctrl-S = save | Ctrl-Q = quit");
 
-    while editor.keep_alive {
+    while editor.keep_alive() {
         editor_refresh_screen(&mut editor);
         editor_process_keypress(&mut editor)?;
     }
